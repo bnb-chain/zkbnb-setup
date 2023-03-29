08cbecd3a48534370ff11869c0a7784ecb463cea
Hisham Galal <0xhgalal@gmail.com>
2023-03-29 18:31:25 -0400

    undo parted r1cs rebase

--- test/example_test.go
+++ test/example_test.go
@@ -1,13 +1,10 @@
 package test
 
 import (
-	"bufio"
-	"encoding/gob"
-	"fmt"
-	bn254r1cs "github.com/consensys/gnark/constraint/bn254"
-	"github.com/consensys/gnark/std/hash/mimc"
 	"os"
 	"testing"
+
+	"github.com/consensys/gnark/std/hash/mimc"
 
 	"github.com/bnbchain/zkbnb-setup/keys"
 	"github.com/bnbchain/zkbnb-setup/phase1"
@@ -88,7 +85,6 @@ func TestSetup(t *testing.T) {
 		t.Error(err)
 	}
 
-
 	// Phase 2 initialization
 	if err := phase2.Initialize("4.ph1", "circuit.r1cs", "0.ph2", "evals.ev"); err != nil {
 		t.Error(err)
@@ -115,118 +111,4 @@ func TestSetup(t *testing.T) {
 	if err := keys.ExtractKeys("1.ph2", "evals.ev"); err != nil {
 		t.Error(err)
 	}
-}
-
-func TestSetupFromFiles(t *testing.T) {
-
-	// Compile the circuit
-	var myCircuit Circuit
-	ccs, err := frontend.Compile(bn254.ID.ScalarField(), r1cs.NewBuilder, &myCircuit)
-	if err != nil {
-		t.Error(err)
-	}
-	nbCons := ccs.GetNbConstraints()
-	batchSize := 10000
-
-	SplitDumpR1CSBinary(ccs.(*bn254r1cs.R1CS), "Foo", 10000)
-
-	var power byte = 9 + Cnt
-
-	// Initialize to Phase 1
-	if err := phase1.Initialize(power, "0.ph1"); err != nil {
-		t.Error(err)
-	}
-
-	// Contribute to Phase 1
-	if err := phase1.Contribute("0.ph1", "1.ph1"); err != nil {
-		t.Error(err)
-	}
-	if err := phase1.Contribute("1.ph1", "2.ph1"); err != nil {
-		t.Error(err)
-	}
-	if err := phase1.Contribute("2.ph1", "3.ph1"); err != nil {
-		t.Error(err)
-	}
-	if err := phase1.Contribute("3.ph1", "4.ph1"); err != nil {
-		t.Error(err)
-	}
-
-	// Verify Phase 1 contributions
-	if err := phase1.Verify("4.ph1"); err != nil {
-		t.Error(err)
-	}
-
-	// Phase 2 initialization
-	if err := phase2.InitializeFromPartedR1CS("4.ph1", "Foo", "0.ph2", "evals.ev", nbCons, batchSize); err != nil {
-		t.Error(err)
-	}
-
-	// Contribute to Phase 2
-	if err := phase2.Contribute("0.ph2", "1.ph2"); err != nil {
-		t.Error(err)
-	}
-
-	// if err := phase2.Contribute("1.ph2", "2.ph2"); err != nil {
-	// 	t.Error(err)
-	// }
-
-	// if err := phase2.Contribute("2.ph2", "3.ph2"); err != nil {
-	// 	t.Error(err)
-	// }
-
-	// Verify Phase 2 contributions
-	if err := phase2.Verify("1.ph2", "0.ph2"); err != nil {
-		t.Error(err)
-	}
-
-	if err := keys.ExtractKeys("1.ph2", "evals.ev"); err != nil {
-		t.Error(err)
-	}
-}
-
-func SplitDumpR1CSBinary(ccs *bn254r1cs.R1CS, session string, batchSize int) error {
-	// E part
-	{
-		ccs2 := &bn254r1cs.R1CS{}
-		ccs2.CoeffTable = ccs.CoeffTable
-		ccs2.R1CSCore.System = ccs.R1CSCore.System
-
-		name := fmt.Sprintf("%s.r1cs.E.save", session)
-		csFile, err := os.Create(name)
-		if err != nil {
-			return err
-		}
-		// cnt, err := ccs2.WriteTo(csFile)
-		// fmt.Println("written ", cnt, name)
-		ccs2.WriteTo(csFile)
-	}
-
-	N := len(ccs.R1CSCore.Constraints)
-	for i := 0; i < N; {
-		// dump R1C[i, min(i+batchSize, end)]
-		ccs2 := &bn254r1cs.R1CS{}
-		iNew := i + batchSize
-		if iNew > N {
-			iNew = N
-		}
-		ccs2.R1CSCore.Constraints = ccs.R1CSCore.Constraints[i:iNew]
-		name := fmt.Sprintf("%s.r1cs.Cons.%d.%d.save", session, i, iNew)
-		csFile, err := os.Create(name)
-		if err != nil {
-			return err
-		}
-		// cnt, err := ccs2.WriteTo(csFile)
-		// fmt.Println("written ", cnt, name)
-		writer := bufio.NewWriter(csFile)
-		enc := gob.NewEncoder(writer)
-		err = enc.Encode(ccs2)
-		if err != nil {
-			panic(err)
-		}
-		//ccs2.WriteTo(csFile)
-
-		i = iNew
-	}
-
-	return nil
 }
